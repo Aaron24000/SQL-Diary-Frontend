df3f885cc58bb6f39b5d2eeb1b2e29c42ea838ac
Aaron24000 <aaron.nobles@outlook.com>
2021-07-29 07:53:14 -0400

    homepage is pretty much done, I'd say

--- sql-diary-frontend/src/components/navbar/Navigation.js
+++ sql-diary-frontend/src/components/navbar/Navigation.js
@@ -1,22 +1,41 @@
-import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  FormControl,
+  Button,
+  Form,
+} from "react-bootstrap";
 
 const Navigation = () => {
-    return (
-        <Navbar fixed="top" bg="transparent" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="/">SQL Diary</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/blogs">Blogs</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
-                        <Nav.Link href="/register">Register</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">SQL Diary</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className=" justify-content-center">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/blogs">Blogs</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search blogs..."
+                className="mr-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Navigation;
--- sql-diary-frontend/src/components/pages/Homepage.js
+++ sql-diary-frontend/src/components/pages/Homepage.js
@@ -1,27 +1,56 @@
 import {
-  Navbar,
   Container,
-  Nav,
-  NavDropdown,
   Row,
   Col,
   Card,
   Button,
 } from "react-bootstrap";
 import { Fragment } from "react";
+import BURDEN from "../photos/aaron-burden-xG8IQMqMITM-unsplash.jpg";
 
 const Homepage = () => {
   return (
     <Fragment>
       <header id="home-heading">
-        <div className="heading-photo text-white">
-          <Container className="pt-5">
-            <Row>
-              <Col className="text-center">
-                <h1>Homepage</h1>
+        <div className="heading-photo">
+          <div className="home-inner container pt-5">
+            <div className="row">
+            <Col md={12}>
+            <h1  className="display-4 text-white pt-5">
+                  Blog Website: <strong>Create</strong>, <strong>share</strong>, and <strong>tell</strong> your story
+                </h1>
+            </Col>
+              <Col md={8}  className="d-block text-white">
+                <div className="d-flex">
+                  <div className="p-4 align-self-start">
+                    <i className="fas fa-chart-bar fa-2x"></i>
+                  </div>
+                  <div className="p-4 align-self-end">
+                  See all of the conferences across the NCAA
+                  </div>
+                </div>
+                <div className="d-flex">
+                  <div className="p-4 align-self-start">
+                    <i className="fas fa-chart-bar fa-2x"></i>
+                  </div>
+                  <div className="p-4 align-self-end">
+                  Search stadiums across 50 states and 30+ conferences!
+                  </div>
+                </div>
+                <div className="d-flex">
+                  <div className="p-4 align-self-start">
+                    <i className="fas fa-chart-bar fa-2x"></i>
+                  </div>
+                  <div className="p-4 align-self-end">
+                  Get team info on individual teams, including team logos!
+                  </div>
+                </div>
               </Col>
-            </Row>
-          </Container>
+              <Col md={4} className="img-fluid d-none d-lg-block pt-5">
+              <img src={BURDEN} style={{ width: '500px'}}></img>
+              </Col>
+            </div>
+          </div>
         </div>
       </header>
 
@@ -33,10 +62,7 @@ const Homepage = () => {
               <h1 className="display-4 text-center">Latest Blogs</h1>
             </Col>
             <Col md={3} className="pb-2">
-              <Card
-                style={{ width: "18rem" }}
-                className="text-center mx-auto"
-              >
+              <Card style={{ width: "18rem" }} className="text-center mx-auto">
                 <Card.Img
                   variant="top"
                   src="https://via.placeholder.com/286x180"
@@ -46,7 +72,9 @@ const Homepage = () => {
                   <Card.Text>
                     Posted by the person who created this, ofc
                   </Card.Text>
-                  <Button variant="primary">Check it out!</Button>
+                  <Button variant="primary" className="rounded-pill">
+                    Check it out!
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
@@ -61,7 +89,9 @@ const Homepage = () => {
                   <Card.Text>
                     Posted by the person who created this, ofc
                   </Card.Text>
-                  <Button variant="primary">Check it out!</Button>
+                  <Button variant="primary" className="rounded-pill">
+                    Check it out!
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
@@ -76,7 +106,9 @@ const Homepage = () => {
                   <Card.Text>
                     Posted by the person who created this, ofc
                   </Card.Text>
-                  <Button variant="primary">Check it out!</Button>
+                  <Button variant="primary" className="rounded-pill">
+                    Check it out!
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
@@ -91,7 +123,9 @@ const Homepage = () => {
                   <Card.Text>
                     Posted by the person who created this, ofc
                   </Card.Text>
-                  <Button variant="danger">Check it</Button>
+                  <Button variant="danger" className="rounded-pill">
+                    Check it
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
@@ -100,27 +134,65 @@ const Homepage = () => {
       </section>
 
       {/* Section with fontawesome stuffs */}
-      <section id="home-fontawesome">
+      <section id="home-fontawesome" className="bg-dark text-white">
         <Container className="pt-4 pb-4">
           <Row>
             <Col md={4} className="text-center pb-5">
-              <i class="fas fa-blog fa-3x"></i>
+              <i class="fas fa-blog fa-3x p-2"></i>
               <h3>Create your blog</h3>
               <p>Create your blog and share your story to the world</p>
             </Col>
             <Col md={4} className="text-center pb-5">
-              <i class="fab fa-facebook fa-3x"></i>
+              <i class="fab fa-facebook fa-3x p-2"></i>
               <h3>Share to Facebook</h3>
               <p>Share your story to facebook to tell your tale to friends.</p>
             </Col>
             <Col md={4} className="text-center">
-              <i class="fas fa-envelope fa-3x"></i>
-              <h3>Something here</h3>
-              <p>Get emails when a new bloog is created.</p>
+              <i class="fas fa-envelope fa-3x p-2"></i>
+              <h3>Get notified</h3>
+              <p>
+                Using the Twilio email API, you'll get emails when a new blog is
+                posted.
+              </p>
             </Col>
           </Row>
         </Container>
       </section>
+
+      {/* About */}
+      <section id="about" className="pt-5 bg-light pb-5">
+        <Container>
+          <Row>
+            <Col md={12}>
+              <h1 className="display-4 text-center">About</h1>
+            </Col>
+            <Col md={12}>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                mollis lorem nisi. Suspendisse pretium est ac mi dignissim
+                finibus. Maecenas vestibulum convallis hendrerit. Quisque
+                vulputate accumsan nibh, et commodo lacus tincidunt mattis.
+                Fusce molestie molestie nibh, feugiat elementum nibh rutrum id.
+                Sed at nisl vestibulum, rhoncus sem eget, aliquam lacus.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      {/* Footer */}
+      <footer id="main-footer" class="text-center text-white bg-dark p-4">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <p>
+              Created by <a rel="noreferrer" href="http://www.aaronnobles.com" target="_blank">Aaron Nobles</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
     </Fragment>
   );
 };
